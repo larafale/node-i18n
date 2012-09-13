230e8178c30dbe3074e4b70a6d50ba911f531df5
Guillaume Denis <guillaume@sevenacademy.com>
2012-09-13 12:24:07 +0200

    vow asynchronous topic

--- test/constants.js
+++ test/constants.js
@@ -3,7 +3,7 @@ var vows = require('vows'),
 
 vows.describe('Translate constants').addBatch({
   'when saying hello in French': {
-    topic: 'Bonjour',
+    topic: function() { this.callback(null, 'Bonjour') },
 
     'we get Bonjour': function (topic) {
       assert.equal(topic, 'Bonjour')
