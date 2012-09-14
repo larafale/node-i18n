7ae126fe9d4893c63cb7ee2c964ba8720f84e4aa
Guillaume Denis <guillaume@sevenacademy.com>
2012-09-14 13:46:23 +0200

    added {{variables}} and tests

--- test/greetings-test.js
+++ test/greetings-test.js
@@ -2,23 +2,42 @@ var vows = require('vows')
   , assert = require('assert')
   , i18n = require('../index')({ dir: './test/fixtures'}).load()
 
-vows.describe('Greetings').addBatch({
-  'when greeting someone': {
-    topic: 'greeting',
-    'in English, we say Hello': function(topic) {
-      assert.equal(i18n.setLocale('en').t(topic), 'Hello')
-    },
-    'in French, we say Bonjour': function(topic) {
-      assert.equal(i18n.setLocale('fr').t(topic), 'Bonjour')
-    },
-    'in the evening': {
-      topic: function(sayWhat) { return 'evening.' + sayWhat },
-      'in English, we say Good evening': function(topic) {
-        assert.equal(i18n.setLocale('en').t(topic), 'Good evening')
+vows.describe('Greetings')
+  .addBatch({
+    'when greeting someone': {
+      topic: 'greeting',
+      'in English, we say Hello': function(topic) {
+        assert.equal(i18n.setLocale('en').t(topic), 'Hello') // classic API
+        assert.equal(i18n.setLocale('fr').t(topic, 'en'), 'Hello') // using a different API
       },
-      'in French, we say Bonsoir': function(topic) {
-        assert.equal(i18n.setLocale('fr').t(topic), 'Bonsoir')
+      'in French, we say Bonjour': function(topic) {
+        assert.equal(i18n.setLocale('fr').t(topic), 'Bonjour')
+      },
+      'in the evening': {
+        topic: function(sayWhat) { return 'evening.' + sayWhat },
+        'in English, we say Good evening': function(topic) {
+          assert.equal(i18n.setLocale('en').t(topic), 'Good evening')
+        },
+        'in French, we say Bonsoir': function(topic) {
+          assert.equal(i18n.setLocale('fr').t(topic), 'Bonsoir')
+        }
       }
     }
-  }
-}).export(module)
+  })
+  .addBatch({
+    'when greeting Billy the kid and Calamity Jane': {
+      topic: function() {
+        this.callback('greetingTwoPeople', {
+          person1: 'Billy the kid',
+          person2: 'Calamity Jane'
+        })
+      },
+      'in English, we say Hello Billy the kid and Calamity Jane': function(sayWhat, toWho) {
+        assert.equal(i18n.setLocale('en').t(sayWhat, toWho), 'Hello Billy the kid and Calamity Jane')
+      },
+      'in French, we say Bonjour Billy the kid et Calamity Jane': function(sayWhat, toWho) {
+        assert.equal(i18n.setLocale('fr').t(sayWhat, toWho), 'Bonjour Billy the kid et Calamity Jane')
+      }
+    }
+  })
+  .export(module)
